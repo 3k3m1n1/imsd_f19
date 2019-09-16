4c3fabcb750c1957a75aeb2d23a1692f876b212f
Ekemini <3k3m1n1@gmail.com>
2019-09-16 02:15:43 -0400

    much better. added wall builder script

---
+++ week-2-hw/Assets/Scripts/WallBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBehavior : MonoBehaviour
+{
+    public GameObject brick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backWallBuilder(-10, 10, 5);
+        sidesWallBuilder(-5, 5, -10);
+        sidesWallBuilder(-5, 5, 10);
+        floorCeilingBuilder(-10, 10, -1);
+        floorCeilingBuilder(-10, 10, 10);
+    }
+
+    void backWallBuilder(int whereToStartWallAlongX, int whereToEndWallAlongX, int zOffset)
+    {
+        for (int lengthOfWall = whereToStartWallAlongX; lengthOfWall <= whereToEndWallAlongX; lengthOfWall++)
+        {
+            for (int heightOfWall = 0; heightOfWall < 10; heightOfWall++)
+            {
+                Instantiate(brick, new Vector3(lengthOfWall, heightOfWall, zOffset), Quaternion.identity);
+            }
+        }
+    }
+
+    void sidesWallBuilder(int whereToStartWallAlongZ, int whereToEndWallAlongZ, int xOffset)
+    {
+        for (int lengthOfWall = whereToStartWallAlongZ; lengthOfWall <= whereToEndWallAlongZ; lengthOfWall++)
+        {
+            for (int heightOfWall = 0; heightOfWall < 10; heightOfWall++)
+            {
+                Instantiate(brick, new Vector3(xOffset, heightOfWall, lengthOfWall), Quaternion.identity);
+            }
+        }
+    }
+
+    void floorCeilingBuilder(int whereToStartWallAlongX, int whereToEndWallAlongX, int yOffset)
+    {
+        for (int lengthOfWall = whereToStartWallAlongX; lengthOfWall <= whereToEndWallAlongX; lengthOfWall++)
+        {
+            for (int widthOfWall = -5; widthOfWall < 5; widthOfWall++)
+            {
+                Instantiate(brick, new Vector3(lengthOfWall, yOffset, widthOfWall), Quaternion.identity);
+            }
+        }
+    }
+}
