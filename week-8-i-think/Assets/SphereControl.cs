323e56fd1e42ac6197cc61ee6e099b970e8bb7a0
Ekemini <3k3m1n1@gmail.com>
2019-10-15 23:25:59 -0400

    hw: control multiple objects, play a clip on another object on key press

---
+++ week-8-i-think/Assets/SphereControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereControl : MonoBehaviour
+{
+    int i;
+    public GameObject[] spheres;
+    public GameObject audioManager;
+
+    void Update()
+    {
+        for (i = 0; i < 5; i++) {
+          // float multiple spheres up and down
+          spheres[i].transform.position = new Vector3(spheres[i].transform.position.x, Mathf.Sin(Time.time), 0);
+        }
+
+        // check to see if key has been pressed
+        IfSpacePlaySound();
+
+    }
+
+    void IfSpacePlaySound() {
+      if (Input.GetKeyDown(KeyCode.Space)) {
+        // grab the audio source from another object
+        AudioSource source = audioManager.GetComponent<AudioSource>();
+        source.Play();
+      }
+    }
+}
