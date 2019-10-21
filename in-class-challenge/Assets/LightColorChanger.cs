9263a640f7fd64488c14086a505aad7ef9bc6790
Ekemini <3k3m1n1@gmail.com>
2019-10-21 10:57:23 -0400

    2nd interactive project: lame attempt at 3d tetris

---
+++ in-class-challenge/Assets/LightColorChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightColorChanger : MonoBehaviour
+{
+    float timer;
+    public GameObject lightObject;
+    Light lightComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 5f) {
+          lightComponent = lightObject.GetComponent<Light>();
+          lightComponent.color = new Color( ( Mathf.Sin(Time.time) / 2) + 0.5f, lightComponent.color.g, lightComponent.color.b, lightComponent.color.a);
+        }
+    }
+}
---
+++ week-9-maybe/Assets/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] blockTypes;
+    GameObject currentBlock;
+    float timer;
+    int rand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      SpawnRandomBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // USER MOVEMENT
+      if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+        // move block one unit to the left
+        currentBlock.transform.position += Vector3.left;
+      }
+      if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        // move block one unit to the right
+        currentBlock.transform.position += Vector3.right;
+      }
+      if (Input.GetKeyDown(KeyCode.Z)) {
+        // rotate 90º counterclockwise
+        currentBlock.transform.Rotate(0f, 0f, 90f);
+      }
+      if (Input.GetKeyDown(KeyCode.X)) {
+        // rotate 90º clockwise
+        currentBlock.transform.Rotate(0f, 0f, -90f);
+      }
+
+      // AUTOMATIC MOVEMENT
+      if (timer >= 1f) {
+        // if the block has not reached the bottom of the map
+        if (currentBlock.transform.position.y > -8f) {
+          currentBlock.transform.position += Vector3.down; // move block down in 1sec ticks
+          timer = 0f; // reset timer, so we can start counting again
+        } else {
+          // the block is now at the bottom. spawn a new one
+          SpawnRandomBlock();
+        }
+      }
+      timer += Time.deltaTime;
+    }
+
+    void SpawnRandomBlock() {
+      rand = Random.Range(0, blockTypes.Length - 1);
+      currentBlock = Instantiate(blockTypes[rand], new Vector3(0, 8, 0), Quaternion.identity);
+    }
+}
