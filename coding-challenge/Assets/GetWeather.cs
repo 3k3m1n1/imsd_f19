c3e0165e2e2007d60dff2aaeccefcf524ddbdfa1
Ekemini <3k3m1n1@gmail.com>
2019-11-18 11:45:03 -0500

    coding challenge: have 3 spheres grow + shrink on key press

---
+++ coding-challenge/Assets/GetWeather.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetWeather : MonoBehaviour
+{
+    string key = "223f922df1b31fbd67b5c41e8b9e7776";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(AskForWeather());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator AskForWeather()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://api.openweathermap.org/data/2.5/weather?zip=11692&mode=xml&APPID=" + key);
+        Debug.Log("Request sent, pending response...");
+        yield return www.SendWebRequest();
+
+        if (!www.isNetworkError && !www.isHttpError)
+            Debug.Log(www.downloadHandler.text);
+        else
+            Debug.Log(www.error + " " + www);
+
+
+    }
+}
---
+++ coding-challenge/Assets/SphereScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour
+{
+    bool is1Growing = true;
+    bool is2Growing = true;
+    bool is3Growing = true;
+
+    public GameObject prefab;
+    Transform[] spheres = new Transform[8];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject g = Instantiate(prefab, new Vector3(2*i, 0f, 0f), Quaternion.identity);
+            spheres[i] = g.transform;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ToggleChangeInSize(KeyCode.Q, spheres[0], is1Growing);
+        ToggleChangeInSize(KeyCode.W, spheres[1], is2Growing);
+        ToggleChangeInSize(KeyCode.E, spheres[2], is3Growing);
+    }
+
+    void ToggleChangeInSize(KeyCode key, Transform sphere, bool isGrowing)
+    {
+        if (Input.GetKey(key))
+        {
+            isGrowing = !isGrowing;
+        }
+
+        if (!isGrowing)
+            sphere.localScale += new Vector3(Time.deltaTime, Time.deltaTime, Time.deltaTime);
+        else
+            if (sphere.localScale.x > 1)
+                sphere.localScale -= new Vector3(Time.deltaTime, Time.deltaTime, Time.deltaTime);
+    }
+}
