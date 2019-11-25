d71412aa242bdab00a2bfeea01603854f6f4f65d
Ekemini <3k3m1n1@gmail.com>
2019-11-25 12:00:00 -0500

    learned lerp function (interpolation)

---
+++ lerp/Assets/LerpScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpScript : MonoBehaviour
+{
+    float timer = 0f;
+    Vector3 start = new Vector3(-5f, 0f, 0f);
+    Vector3 end = new Vector3(0f, 0f, -5f);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = Mathf.PingPong(Time.time, 1f);
+
+        // transform.position = (end - start) * timer + start;
+        // or
+        transform.position = Vector3.Lerp(start, end, timer);
+    }
+}
